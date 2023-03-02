426424f5bdb0814cb5e8cb0a87f4a3dcd9f9121d
a1113361 <125753947+a1113361@users.noreply.github.com>
2023-03-02 18:31:13 +0800

    Add files via upload

---
+++ A1113361_0224_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class A1113361_0224_1{
+	public static void main (String [] argv){
+		int num=0;
+
+	System.out.println("請輸入任意正整數:");
+	Scanner sc = new Scanner(System.in);
+	num = sc.nextInt();
+	System.out.print((num%2==0)?"是偶數":"是奇數");
+
+    }
+}
