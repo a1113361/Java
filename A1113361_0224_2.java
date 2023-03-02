7590091abd43fcd5eb49abcefe355018d5a08c43
a1113361 <125753947+a1113361@users.noreply.github.com>
2023-03-02 18:40:04 +0800

    Add files via upload

---
+++ A1113361_0224_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A1113361_0224_2{
+	public static void main (String [] argv){
+		float C, F;
+
+	System.out.println("請輸入代表攝氏溫度的值:");
+	Scanner sc = new Scanner(System.in);
+	C = sc.nextInt();
+	F = C*9/5+32;
+	System.out.print("華氏溫度為"+F+"度");
+
+    }
+}
